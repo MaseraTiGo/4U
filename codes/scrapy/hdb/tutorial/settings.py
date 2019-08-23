da8d093bf78e5eee36a7adbec1747b2d2cf2ad5d
johnathan.wick <2242276320@qq.com>
2019-08-23 17:49:50 +0800

    test

--- codes/scrapy/hdb/tutorial/settings.py
+++ codes/scrapy/hdb/tutorial/settings.py
@@ -89,5 +89,5 @@ ITEM_PIPELINES = {
 # HTTPCACHE_IGNORE_HTTP_CODES = []
 # HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
-IMAGES_STORE = 'fuck you'
+IMAGES_STORE = 'fuck'
 IMAGES_URLS_FIELD = 'pics'
