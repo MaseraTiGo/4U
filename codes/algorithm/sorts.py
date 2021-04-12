c5a242e0b5c1b2ec001c2f0e0c6773ce28f2fe7d
jundong.dong <jundong.dong@cy-tech.net>
2021-04-12 14:51:49 +0800

    add insertion_sort and selection_sort

--- codes/algorithm/sorts.py
+++ codes/algorithm/sorts.py
@@ -1,6 +1,40 @@
 from typing import List, Optional
 
 
+# ================================================= common funcs =======================================================
+
+
+# selection sort
+def selection_sort(src_list):
+    minimum = float('inf')
+    for i in range(len(src_list) - 1):
+        for item in src_list[i:]:
+            minimum = min(minimum, item)
+        j = src_list.index(minimum)
+        src_list[i], src_list[j] = src_list[j], src_list[i]
+        minimum = float('inf')
+    return src_list
+
+
+# insert sort
+def insertion_sort(src_list):
+    src_len = len(src_list)
+    for i in range(src_len):
+        current = src_list[i]
+        animation_pos = i
+        while animation_pos > 0 and src_list[animation_pos - 1] > current:
+            src_list[animation_pos] = src_list[animation_pos - 1]
+            animation_pos -= 1
+        src_list[animation_pos] = current
+    return src_list
+
+
+print(insertion_sort([1, 4, 3, 5, 2, 7, 9, 6, 8]))
+
+
+# ================================================= common funcs =======================================================
+
+
 # ================================================= 1528. Shuffle String ===============================================
 
 class Solution1528:
