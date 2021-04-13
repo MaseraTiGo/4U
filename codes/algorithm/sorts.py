527b0c9cf929f67951162a9157427086fd5eb287
jundong.dong <jundong.dong@cy-tech.net>
2021-04-13 11:45:23 +0800

    add common sort func: merge_sort ps: need further understand

--- codes/algorithm/sorts.py
+++ codes/algorithm/sorts.py
@@ -1,4 +1,11 @@
 from typing import List, Optional
+import random
+
+
+def give_me_some_random_nums(num=7, start=0, end=99):
+    result = [random.randint(start, end) for _ in range(num)]
+    print(f'dong ------------------->random nums: {result}')
+    return result
 
 
 # ================================================= common funcs =======================================================
@@ -58,7 +65,6 @@ def shell_sort(src_list):
         for i in range(gap, n):
             temp = src_list[i]
             j = i - gap
-            print(f'dong ------------>{i, j, gap}')
             while j >= 0 and src_list[j] > temp:
                 src_list[j + gap] = src_list[j]
                 j = j - gap
@@ -67,7 +73,47 @@ def shell_sort(src_list):
     return src_list
 
 
-print(shell_sort([1, 4, 3, 5, 2, 7, 9, 6, 8]))
+# merge sort
+def merge_core(list_one, list_two=None):
+    if not list_two:
+        return list_one
+    temp = []
+
+    i = j = 0
+    while 1:
+        if i == len(list_one) or j == len(list_two):
+            break
+        if list_one[i] < list_two[j]:
+            temp.append(list_one[i])
+            i += 1
+        elif list_one[i] == list_two[j]:
+            temp.extend([list_one[i]] * 2)
+            i += 1
+            j += 1
+        else:
+            temp.append(list_two[j])
+            j += 1
+    temp.extend(list_one[i:])
+    temp.extend(list_two[j:])
+    return temp
+
+
+# print(merge_core([1, 3, 5, 5, 7, 9], [0, 2, 4, 6, 6, 8]))
+
+merge_ans = []
+
+
+def merge_sort(src_list):
+    if len(src_list) <= 1:
+        return src_list
+    mid = int(len(src_list) // 2)
+    left = merge_sort(src_list[: mid])
+    right = merge_sort(src_list[mid:])
+    res = merge_core(left, right)
+    return res
+
+
+print(merge_sort(give_me_some_random_nums()))
 
 
 # ================================================= common funcs =======================================================
