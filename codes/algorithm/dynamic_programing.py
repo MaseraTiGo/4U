4b92dfcbddadec148cb66a4c266ec325e39eaf51
jundong.dong <jundong.dong@cy-tech.net>
2021-04-20 11:39:55 +0800

    add leetcode-dp-64

--- codes/algorithm/dynamic_programing.py
+++ codes/algorithm/dynamic_programing.py
@@ -13,6 +13,10 @@
 from codes.algorithm import print_
 
 
+def get_my_dp(m, n):
+    return [[None for _ in range(n)] for _ in range(m)]
+
+
 # ================================================= 62. Unique Paths ===================================================
 
 # Runtime: 28 ms, faster than 84.82% of Python3 online submissions for Unique Paths.
@@ -39,3 +43,33 @@ class Solution62:
 
 # print(Solution62().solve(3, 3))
 # ================================================= 62. Unique Paths ===================================================
+
+
+# ================================================= 64. Minimum Path Sum ===============================================
+
+
+# Runtime: 100 ms, faster than 52.92% of Python3 online submissions for Minimum Path Sum.
+# Memory Usage: 16.2 MB, less than 15.21% of Python3 online submissions for Minimum Path Sum.
+class Solution64:
+    def solve(self, grid):
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[None for _ in range(n)] for _ in range(m)]
+        dp[0][0] = grid[0][0]
+        for i in range(1, m):
+            dp[i][0] = dp[i - 1][0] + grid[i][0]
+        for j in range(1, n):
+            dp[0][j] = dp[0][j - 1] + grid[0][j]
+
+        def helper(m, n):
+            if dp[m][n] is not None:
+                return dp[m][n]
+            dp[m][n] = min(helper(m - 1, n), helper(m, n - 1)) + grid[m][n]
+            return dp[m][n]
+
+        ans = helper(m - 1, n - 1)
+        return ans
+
+
+# print(Solution64().solve([[1, 2, 3], [4, 5, 6]]))
+# ================================================= 64. Minimum Path Sum ===============================================
