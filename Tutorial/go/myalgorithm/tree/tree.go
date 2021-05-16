e988c8244fc05f48c28b1d3aedef9703d79eb841
johnathan.wick <2242276320@qq.com>
2021-05-16 10:52:43 +0800

    add leetcode-tree-590-bygo

--- Tutorial/go/myalgorithm/tree/tree.go
+++ Tutorial/go/myalgorithm/tree/tree.go
@@ -74,6 +74,7 @@ type Node struct {
 	Children []*Node
 }
 
+// ---------------------------- Preorder589 ---------------------------------
 func helper589(node *Node, ans *[]int) {
 	if node != nil {
 		*ans = append(*ans, node.Val)
@@ -84,8 +85,10 @@ func helper589(node *Node, ans *[]int) {
 }
 
 
-// Runtime: 0 ms, faster than 100.00% of Go online submissions for N-ary Tree Preorder Traversal.
-// Memory Usage: 4 MB, less than 31.50% of Go online submissions for N-ary Tree Preorder Traversal.
+// Runtime: 0 ms, faster than 100.00% of Go online submissions for N-ary Tree
+// Preorder Traversal.
+// Memory Usage: 4 MB, less than 31.50% of Go online submissions for N-ary
+// Tree Preorder Traversal.
 func Preorder589(root *Node) []int {
 	if root == nil {
 		return nil
@@ -95,9 +98,40 @@ func Preorder589(root *Node) []int {
 
 	return ans
 }
+// ---------------------------- Preorder589 ---------------------------------
 
-// Runtime: 24 ms, faster than 90.05% of Go online submissions for Merge Two Binary Trees.
-//Memory Usage: 8.4 MB, less than 38.74% of Go online submissions for Merge Two Binary Trees.
+
+// ---------------------------- Postorder590 --------------------------------
+func helper590(node *Node, ans *[]int) {
+	if node != nil {
+		for _, cNode := range node.Children {
+			helper590(cNode, ans)
+		}
+		*ans = append(*ans, node.Val)
+	}
+}
+
+// Runtime: 0 ms, faster than 100.00% of Go online submissions for N-ary Tree
+// Postorder Traversal.
+// Memory Usage: 4.3 MB, less than 40.00% of Go online submissions for N-ary
+// Tree Postorder Traversal.
+func Postorder(root *Node) []int {
+	if root == nil {
+		return nil
+	}
+
+	var ans []int
+	helper590(root, &ans)
+	return ans
+}
+// ---------------------------- Postorder590 --------------------------------
+
+
+// ---------------------------- MergeTrees617 -------------------------------
+// Runtime: 24 ms, faster than 90.05% of Go online submissions for Merge Two
+// Binary Trees.
+// Memory Usage: 8.4 MB, less than 38.74% of Go online submissions for Merge
+// Two Binary Trees.
 func MergeTrees617(root1 *TreeNode, root2 *TreeNode) *TreeNode {
 	var current *TreeNode
 	if root1 != nil && root2 != nil {
@@ -116,7 +150,10 @@ func MergeTrees617(root1 *TreeNode, root2 *TreeNode) *TreeNode {
 	}
 	return current
 }
+// ---------------------------- MergeTrees617 -------------------------------
 
+
+// ---------------------------- IncreasingBST897 ----------------------------
 func helper897(node *TreeNode, current *TreeNode) *TreeNode {
 	if node == nil {
 		return current
@@ -128,15 +165,20 @@ func helper897(node *TreeNode, current *TreeNode) *TreeNode {
 	return current
 }
 
-// Runtime: 0 ms, faster than 100.00% of Go online submissions for Increasing Order Search Tree.
-// Memory Usage: 2.5 MB, less than 54.55% of Go online submissions for Increasing Order Search Tree.
+// Runtime: 0 ms, faster than 100.00% of Go online submissions for Increasing
+// Order Search Tree.
+// Memory Usage: 2.5 MB, less than 54.55% of Go online submissions for
+// Increasing Order Search Tree.
 func IncreasingBST897(root *TreeNode) *TreeNode {
 	head := &TreeNode{}
 	current := head
 	helper897(root, current)
 	return head.Right
 }
+// ---------------------------- IncreasingBST897 ----------------------------
 
+
+// ---------------------------- RangeSumBST938 ------------------------------
 func helper938(node *TreeNode, ans *int, low int, high int) {
 	if node != nil {
 		if low <= node.Val && node.Val <= high {
@@ -147,8 +189,10 @@ func helper938(node *TreeNode, ans *int, low int, high int) {
 	}
 }
 
-// Runtime: 84 ms, faster than 91.67% of Go online submissions for Range Sum of BST.
-//Memory Usage: 9 MB, less than 5.11% of Go online submissions for Range Sum of BST.
+// Runtime: 84 ms, faster than 91.67% of Go online submissions for Range
+// Sum of BST.
+//Memory Usage: 9 MB, less than 5.11% of Go online submissions for Range
+// Sum of BST.
 func RangeSumBST938(root *TreeNode, low int, high int) int {
 	//ans := 0
 	//nodeList := []*TreeNode{root}
@@ -173,3 +217,4 @@ func RangeSumBST938(root *TreeNode, low int, high int) int {
 	helper938(root, &ans, low, high)
 	return ans
 }
+// ---------------------------- RangeSumBST938 ------------------------------
